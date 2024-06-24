0fe73884f2bfb5142ca63e5102aa8c7e585184fb
tobsolutions <tobi@tobsolutions.de>
2024-06-24 18:18:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require __DIR__ . '/vendor/autoload.php';
 use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\Printer;
-$connector = new FilePrintConnector("php://stdout");
+//$connector = new FilePrintConnector("php://stdout");
+$connector = new NetworkPrintConnector("192.168.1.25", 9100);
 $printer = new Printer($connector);
 $printer -> text("Hello World!\n");
 $printer -> cut();
