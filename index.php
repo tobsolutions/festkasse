9c00ea5090845f287a9aab69430208b31fe44911
tobsolutions <tobi@tobsolutions.de>
2024-06-24 18:56:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /* Call this file 'hello-world.php' */
-require __DIR__ . '/vendor/autoload.php';
-use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
+require '/vendor/autoload.php';//__DIR__ . '/vendor/autoload.php';
 use Mike42\Escpos\Printer;
-//$connector = new NetworkPrintConnector("192.168.10.25", 9100);
+use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
+$connector = new NetworkPrintConnector("192.168.10.25", 9100);
 $printer = new Printer($connector);
-/*try {
+try {
     $printer -> text("Hello World!\n");
 } finally {
     $printer -> close();
-}*/
+}
