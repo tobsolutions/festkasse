ce47bf77d7750b72bab8dd4a3b986bb7dc855e35
tobsolutions <tobi@tobsolutions.de>
2024-06-24 18:59:54 +0200

    test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 /* Call this file 'hello-world.php' */
-require __DIR__ . 'autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+echo "require: " . __DIR__ . '/vendor/autoload.php';
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 $connector = new NetworkPrintConnector("192.168.10.25", 9100);
